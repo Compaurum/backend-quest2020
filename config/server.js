7ecc07a047f53e9e81babca2a49e3482db36771b
Denis Ivanov <di@madappgang.com>
2020-08-22 14:14:46 +0300

    initial commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '410ddea3a6365898a426483505fe2142'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'c99f65b8-ba9f-4009-88e9-bb16f9567aba'
+};
